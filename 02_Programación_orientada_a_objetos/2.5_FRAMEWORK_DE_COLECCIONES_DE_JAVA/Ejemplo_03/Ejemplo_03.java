0b4a5a84a9c20d5e055c2556d39d5e6b4728b6c0
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2025-09-11 18:31:35 +0200

    Capítulo_02 (2.5.5. LAS CLASES Collections y Arrays, 2.5.5.1. Convertir arrays en List y viceversa)

---
+++ 02_Programación_orientada_a_objetos/2.5_FRAMEWORK_DE_COLECCIONES_DE_JAVA/Ejemplo_03/Ejemplo_03.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Ejemplo_03 {
+    public static void main(String[] args) {
+        // 2.5.5.1. Convertir arrays en List y viceversa
+        // En este ejemplo convertimos un array primitivo en una instancia de List
+        // as.List sería como un factory method
+        Integer[] arr1 = {1,2,3,4};
+        List<Integer> list1 = Arrays.asList(arr1);
+        System.out.println(list1);
+
+        // También podemos obtener una lista a partir de un conjunto de elementos
+        List<Integer> list2 = Arrays.asList(1,2,3,4);
+        System.out.println(list2);
+        System.out.println();
+
+        // El caso invertido será convertir una instancia de List en un array primitivo
+        List<Integer> list = Arrays.asList(1,2,3,4);
+        Integer[] arr2 = (Integer[])list.toArray();
+    }
+}
