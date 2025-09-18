6750fbcec16e8ec0c9b040422ef2b8bba0fdbdec
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2025-09-18 21:16:55 +0200

    Capítulo_10 (10.2.1. LA INTERFACE Runnable)

--- 10_THREADS:_MULTIPROGRAMACIÓN/10_02_PROGRAMAR_CON_THREADS/Ejemplo_01/HolaMundoThread.java
+++ 10_THREADS:_MULTIPROGRAMACIÓN/10_02_PROGRAMAR_CON_THREADS/Ejemplo_01/HolaMundoThread.java
@@ -1,8 +1,8 @@
-// Un thread es una instancia de alguna clase que extiende a Thread y sobreescribe el método run. Este método es secuencial.
+// Un thread es una instancia de alguna clase que extiende a Thread y sobreescribe el método run. Este método es secuencial
 // Es allí donde debemos programar la tarea que el proceso concurrente llevará a cabo:
 // El siguiente ejemplo, extiende a Thread y sobreescribe el método run para hacer lo siguiente:
-// - Duerme una cantidad aleatoria de milisengundo(0 y 4999).
-// - Cuando se despierta escribe en pantalla el nombre que recibió como parámetro en el constructor y muestra cuánto tiempo durmió.
+// - Duerme una cantidad aleatoria de milisengundo(0 y 4999)
+// - Cuando se despierta escribe en pantalla el nombre que recibió como parámetro en el constructor y muestra cuánto tiempo durmió
 
 public class HolaMundoThread extends Thread {
     private String nombre;
@@ -29,7 +29,7 @@ public class HolaMundoThread extends Thread {
         HolaMundoThread t2 = new HolaMundoThread("Pedro");
         HolaMundoThread t3 = new HolaMundoThread("Juan Pedro");
 
-        // Y los ejecutamos concurrentemente, invocando sobre cada uno, el método start.
+        // Y los ejecutamos concurrentemente, invocando sobre cada uno, el método start
         // Este método run que sobreescribimos en HolaMundoThread
         t1.start();
         t2.start();
@@ -37,10 +37,10 @@ public class HolaMundoThread extends Thread {
 
         System.out.println();
 
-        // La salida será aleatoria, pues cada thread dormirá una cantidad diferente de milisengundos. Por lo tanto, quien duerma menos tiempo será el primero en escribir su nombre.
-        // IMPORTANTE: para que estos hilos sean ejecutados concurrentemente, tenemos que invocar el método start. Este será quien invoque el método run.
+        // La salida será aleatoria, pues cada thread dormirá una cantidad diferente de milisengundos. Por lo tanto, quien duerma menos tiempo será el primero en escribir su nombre
+        // IMPORTANTE: para que estos hilos sean ejecutados concurrentemente, tenemos que invocar el método start. Este será quien invoque el método run
 
-        // Si en lugar de invocar start invocamos run el programa será lineal y los métodos run de cada hilo se ejecutarán secuencialmente, en el orden en el que fueron invocados.
+        // Si en lugar de invocar start invocamos run el programa será lineal y los métodos run de cada hilo se ejecutarán secuencialmente, en el orden en el que fueron invocados
         t1.run();
         t2.run();
         t3.run();
---
+++ 10_THREADS:_MULTIPROGRAMACIÓN/10_02_PROGRAMAR_CON_THREADS/Ejemplo_02/HolaMundoThread.java
@@ -0,0 +1,34 @@
+// En realidad Thread implementa la interface Runnable. De esta hereda el método run
+// En este ejemplo en vez de extender de Thread implementamos la interface Runnable
+
+public class HolaMundoThread implements Runnable {
+    private String nombre;
+
+    public HolaMundoThread(String nobmre) {
+        this.nombre = nombre;
+    }
+
+    public void run() {
+        try {
+            int x = (int)(Math.random() * 5000);
+            System.out.println();
+            Thread.sleep(x);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // En el main en vez de los tres HolaMundoThread, instanciamos directamente tres Thread
+    // Donde cada uno recibe en su constructor una instancia de Runnable, es decir la instancia de HolaMundoThread
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new HolaMundoThread("Juan"));
+        Thread t2 = new Thread(new HolaMundoThread("Pedro"));
+        Thread t3 = new Thread(new HolaMundoThread("Juan Pedro"));
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        // Ambas versiones del programa son equivalentes, sin embargo la segunda es más flexible, pues no restringe la herencia de la clase
+    }
+}
