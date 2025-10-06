0175bd249f7bbc04a20875646ed81f2d10efe68f
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2025-10-06 23:42:42 +0200

    Capítulo_03 (3.4. FUNCIONES, PREDICADOS Y CONSUMIDORES)

---
+++ 03_Lambda_streams_y_programación_funcional/3_4_EXPRESIONES_PREDICADOS_Y_CONSUMIDORES/ListUtil.java
@@ -0,0 +1,14 @@
+// 3.4. FUNCIONES, PREDICADOS Y CONSUMIDORES
+// Existen interfaces genéricas, diseñadas para evitar que escribamos nuestras propias interfaces funcionales. Solo tendremos que escoger cuál utilizar e implementar mediante una expresión lambda.
+// La interfaces funcionales de java.util.function se dividen en tres grupos:
+// Functions: funciones cuyo valor de retorno no es void ni boolean.
+// Predicates: para representar funciones cuyo valor de retorno es boolean.
+// Consumers: para representar funciones que not tienen valor de retorno.
+
+/* Los ejemplos que plantearemos a continuación los vamos a utilzar para desarrollar métodos estáticos
+ * que nos permitirán manipular funcionalmente colecciones de objetos.
+ * Estos métodos son los que iremos incorporando a una clase utilitaria que llamaremos ListUtil
+ */
+
+public class ListUtil {
+}
